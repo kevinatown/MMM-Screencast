577125236a922d50c34e1b27d8c44589fd72b334
kevinatown <kevinatown@gmail.com>
2019-11-04 11:05:05 -0500

    added useragent default

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const electron = require('electron');
+const electron, { session } = require('electron');
 const Positioner = require('electron-positioner');
 const ipc = require('node-ipc');
 ipc.config.id = 'screenCastWindow';
@@ -7,9 +7,13 @@ ipc.config.socketRoot = 'tmp';
 ipc.config.networkHost = 'localhost';
 ipc.config.appSpace = 'MMM-Screencast';
 const url = process.argv[2];
-const position = process.argv[3]
-const width = parseInt(process.argv[4], 10)
-const height = parseInt(process.argv[5], 10)
+const position = process.argv[3];
+const width = parseInt(process.argv[4], 10);
+const height = parseInt(process.argv[5], 10);
+
+// more useragents here: https://developers.whatismybrowser.com/useragents/explore/operating_platform/smart-tv/
+const userAgent = 'Mozilla/5.0 (SMART-TV; X11; Linux armv7l) AppleWebKit/537.42 (KHTML, like Gecko) Chromium/25.0.1349.2 Chrome/25.0.1349.2 Safari/537.42';
+
 
 ipc.serve(`/${ipc.config.socketRoot}/${ipc.config.appSpace}.${ipc.config.id}`, () => {
   ipc.server.on('quit', (data, socket) => {
@@ -18,7 +22,11 @@ ipc.serve(`/${ipc.config.socketRoot}/${ipc.config.appSpace}.${ipc.config.id}`, (
     process.exit();
   });
 });
+
+
+
 const app = electron.app;
+session.defaultSession.setUserAgent(userAgent);
 
 app.once('ready', function () {
   const windowOptions = {
