bf6129d0feeb0c14079da8a00053c20fc81db4a7
Kevin Townsend <ktownsend@medallia.com>
2019-12-05 18:26:48 -0500

    something?

--- DialServer.js
+++ DialServer.js
@@ -70,11 +70,11 @@ const App = function() {
 	        app.state = "starting";
 	        app.launch(lauchData, this.electronConfig);
 
-	        this.mmSendSocket('MMM-Screencast_LAUNCH-APP', { app: app.name, state: app.state });
+	        this.mmSendSocket('MMM-Screencast:LAUNCH-APP', { app: app.name, state: app.state });
 
 	        app.ipc.on('APP_READY' () => {
 	        	app.state = "running";
-	        	this.mmSendSocket('MMM-Screencast_RUN-APP', { app: app.name, state: app.state });
+	        	this.mmSendSocket('MMM-Screencast:RUN-APP', { app: app.name, state: app.state });
 	        });
 	    		
 	      }
@@ -94,7 +94,7 @@ const App = function() {
 	        });
 	        app.ipc.emit('quit');
 
-	        this.mmSendSocket('MMM-Screencast_STOP-APP', { app: app.name, state: app.state });
+	        this.mmSendSocket('MMM-Screencast:STOP-APP', { app: app.name, state: app.state });
 	        child = null;
 
 	        callback(true);
